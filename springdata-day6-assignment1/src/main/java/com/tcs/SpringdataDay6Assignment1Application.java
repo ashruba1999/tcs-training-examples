36b1c60ee87ed333b0d0069613fd97b845100186
ashruba <ashrubajogdand4@gmail.com>
2021-10-25 18:25:28 +0530

    spring data

---
+++ springdata-day6-assignment1/src/main/java/com/tcs/SpringdataDay6Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataDay6Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataDay6Assignment1Application.class, args);
+	}
+
+}
---
+++ springdata-day6-assignment1/src/test/java/com/tcs/SpringdataDay6Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataDay6Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
